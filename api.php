df24b7b3156276eccf0c07ea962258df8e28f662
Md-shefat-masum <mshefat924@gmail.com>
2023-06-22 16:20:03 +0600

    API Testing

--- api.php
+++ api.php
@@ -1,5 +1,24 @@
-Route::get('/user', function () {
+function get_data()
+{
+    if(!file_exists(public_path('d.json'))){
+        file_put_contents('d.json',"[]");
+    }
     $data = json_decode(file_get_contents(public_path('d.json')));
+    $data = array_reverse($data);
+    return $data;
+}
+Route::get('/user', function () {
+    $data = get_data();
+    if(request()->has('page')){
+        $page = request()->page;
+        $per_page = 10;
+        $items = array_chunk($data, $per_page)[$page-1];
+        $paginate = new \Illuminate\Pagination\LengthAwarePaginator($items,count($data),$per_page,$page, [
+            "path" => "/api/user"
+        ]);
+        $paginate_html = $paginate->render();
+        return response()->json([$paginate, $paginate_html]);
+    }
     return response()->json($data);
 });
 
@@ -22,13 +41,12 @@ Route::post('/user', function () {
         ], 422);
     }
 
-    $data = json_decode(file_get_contents(public_path('d.json')));
-    $req_data['id'] = count($data) + 1;
+    $data = get_data();
+    $req_data['id'] = count($data)+1;
     $req_data = array_merge($req_data, request()->all());
     $data[] = $req_data;
 
     file_put_contents(public_path('d.json'), json_encode($data, JSON_PRETTY_PRINT));
-
     $data = array_reverse($data);
     return response()->json($data);
 });
@@ -50,8 +68,8 @@ Route::put('/user/{id}', function ($id) {
             'data' => $validator->errors(),
         ], 422);
     }
-    $data = json_decode(file_get_contents(public_path('d.json')));
-    $index = array_search($id, array_column($data, 'id'));
+    $data = get_data();
+    $index = array_search($id,array_column($data,'id'));
     $item = $data[$index];
     foreach (request()->all() as $key => $value) {
         $item->$key = $value;
@@ -62,8 +80,8 @@ Route::put('/user/{id}', function ($id) {
 });
 
 Route::delete('/user/{id}', function ($id) {
-    $data = json_decode(file_get_contents(public_path('d.json')));
-    $index = array_search($id, array_column($data, 'id'));
+    $data = get_data();
+    $index = array_search($id,array_column($data,'id'));
     unset($data[$index]);
     file_put_contents(public_path('d.json'), json_encode($data, JSON_PRETTY_PRINT));
     return response()->json(["$id deleted", $data]);
